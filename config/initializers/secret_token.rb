d3c5a7a47370aa705c86dde753fd8ae7f8d3d4ab
Selectable <selectable@Selectables-MacBook-Pro-2.local>
2014-09-25 18:10:07 -0400

    initial commit of demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '843e29d10c9865720c282ada38f399652eb1dfa51bba13ba884ad5e1a71a8e856885f418e5fd69c821f41fc29a1718caddc8f20b8d4f89642fdaae28f2e178cd'
